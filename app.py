3a5579267cd7d1794763cb065ac378587536cbcc
Xiuyuan Yang <xiu@Xiuyuans-MacBook-Air.local>
2017-12-12 19:28:58 -0500

    first commit

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  #app.run(port=33507)
+  port = int(os.environ.get('port', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
