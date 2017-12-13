baa656decfee5eec3ed7a05074e85f22c7d9f719
Xiuyuan Yang <xiu@Xiuyuans-MacBook-Air.local>
2017-12-12 23:27:33 -0500

    first commit

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('port', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-    #app.run(host='0.0.0.0', port=port, debug=False)
+    #app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
